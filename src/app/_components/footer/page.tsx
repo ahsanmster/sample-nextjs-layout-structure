11dabb32c2d2ad05ec2e1456abfd8d40d8cb9a2b
Ahsan Hussain <ahsanmster@gmail.com>
2025-08-29 03:10:43 +0500

    layout basic setup

---
+++ src/app/_components/footer/page.tsx
@@ -0,0 +1,11 @@
+export default function Footer(){
+    return (
+        <div style={{
+            backgroundColor: "ghostWhite",
+            padding: "3rem",
+            color: "black"
+        }}>
+            Footer
+        </div>
+    );
+}
---
+++ src/app/_components/header/page.tsx
@@ -0,0 +1,11 @@
+export default function Header(){
+    return (
+        <div style={{
+            backgroundColor: "lightBlue",
+            padding: "3rem",
+            color: "black"
+        }}>
+            Header
+        </div>
+    );
+}
